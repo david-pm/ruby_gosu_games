84415f9a328b6ca3884b9a54a39c3af6956a1d73
davidpm <davidpm.io.web@gmail.com>
2016-07-25 23:01:04 -0700

    moving ruby in window

---
+++ wack_a_ruby.rb
@@ -0,0 +1,28 @@
+require "gosu"
+
+class WackARuby < Gosu::Window
+
+  def initialize(width, height)
+    super(width, height)
+    self.caption = "Wack A Ruby!"
+
+    @image  = Gosu::Image.new("ruby.png")
+
+    @x_offset, @y_offset, @width, @height  = 150, 150, 150, 150
+    @velocity_x, @velocity_y = 5, 5
+  end
+
+  def draw
+    @image.draw(@x_offset - @width / 2, @y_offset - @height / 2, 1)
+  end
+
+  def update
+    @x_offset += @velocity_x
+    @y_offset += @velocity_y
+    @velocity_x *= -1 if @x_offset + (@width / 2) > 800  || @x_offset - (@width / 2) < 0
+    @velocity_y *= -1 if @y_offset + (@height / 2) > 600 || @y_offset - (@height / 2) < 0
+  end
+end
+
+window = WackARuby.new(800, 600)
+window.show
