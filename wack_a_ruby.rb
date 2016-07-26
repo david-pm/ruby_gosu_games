c86fede617d6ddca8dd1c4133226fac9f62699bb
davidpm <davidpm.io.web@gmail.com>
2016-07-25 23:47:14 -0700

    ruby blink

--- wack_a_ruby.rb
+++ wack_a_ruby.rb
@@ -11,10 +11,13 @@ class WackARuby < Gosu::Window
 
     @x_offset, @y_offset, @width, @height  = 150, 150, 150, 150
     @velocity_x, @velocity_y = 4, 4
+    @visible = 0
   end
 
   def draw
-    @ruby.draw(@x_offset - @width / 2, @y_offset - @height / 2, 1)
+    if @visible > 0
+      @ruby.draw(@x_offset - @width / 2, @y_offset - @height / 2, 1)
+    end
     @dude.draw(150, 150, 1)
   end
 
@@ -23,6 +26,8 @@ class WackARuby < Gosu::Window
     @y_offset += @velocity_y
     @velocity_x *= -1 if @x_offset + (@width / 4) > 800  || @x_offset - (@width / 4) < 0
     @velocity_y *= -1 if @y_offset + (@height / 4) > 600 || @y_offset - (@height / 4) < 0
+    @visible -= 1
+    @visible = 30 if @visible < -10 && rand < 0.01
   end
 end
 
